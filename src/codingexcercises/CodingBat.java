e6f9d8649245d67cbe4bdc9925ecb08c2afbaddb
Luis Velasco <luis.velasco.c@outlook.com>
2019-12-18 00:52:34 -0500

    print output of makebricks problem and simplify the last if else

--- src/codingexcercises/CodingBat.java
+++ src/codingexcercises/CodingBat.java
@@ -3,20 +3,21 @@ package codingexcercises;
 public class CodingBat {
 
 	public static void main(String[] args) {
-		boolean canMakeBrick = makeBricks(3, 2, 8);
-		
-		System.out.println(8 / 5);
-		System.out.println(10 / 5);
+		int small = 2;
+		int big = 2;
+		int goal = 8;
+		boolean canMakeBrick = makeBricks(small, big, goal);
+
+		System.out.println("small = " + small + " big = " + big + " goal = " + goal);
+		System.out.println(canMakeBrick);
 	}
-	
-		
+
+
 	public static boolean makeBricks(int small, int big, int goal) {
-	    int bigSum = big * 5;
-	    
-	    if(bigSum + small < goal){
-	      return false;
-	    }else{
-	      return true;
-	    }
+		int bigSum = big * 5;
+
+		if(bigSum + small < goal){
+			return false;
+		}else return goal % 5 <= small;
 	}
 }
