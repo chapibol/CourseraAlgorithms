a703b2ac47bf92c3002e901d949ba4b0d64a1bdb
Velasco Claros <l.a.velasco.claros@accenturefederal.com>
2020-01-23 11:21:11 -0500

    add correct xyz method and add starter get sandwich method

--- src/codingexcercises/CodingBat.java
+++ src/codingexcercises/CodingBat.java
@@ -20,4 +20,34 @@ public class CodingBat {
 			return false;
 		}else return goal % 5 <= small;
 	}
+
+	public static boolean xyzMiddle(String str) {
+		if(str.length() >= 3){
+			//determine if even or odd
+			int end = (str.length() / 2) + 2;
+			if(str.length() % 2 == 0){
+				int begin = (str.length() / 2) - 2;
+				return str.substring(begin, end).contains("xyz");
+			}else{
+				int begin = (str.length() / 2) - 1;
+				return str.substring(begin, end).equals("xyz");
+			}
+		}else {
+			return false;
+		}
+	}
+
+	public String getSandwich(String str) {
+
+		int i = 0;
+		String locationStr = "";
+		while(i < str.length() && i != -1){
+			i = str.indexOf("bread", i);
+			locationStr = locationStr + i;
+			i = i + 5;
+		}
+
+		return locationStr;
+
+	}
 }
