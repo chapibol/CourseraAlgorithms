2e1dd3b4ab7f4ec5b86c234e0a3b3f52e42941f6
Luis Velasco <luis.velasco.c@outlook.com>
2020-01-29 17:28:29 -0500

    add a class to group value, weight and itemValue per item.  left comment to sort this array

--- src/algorithmsweek3/codingproblems/FractionalKnapsack.java
+++ src/algorithmsweek3/codingproblems/FractionalKnapsack.java
@@ -3,9 +3,17 @@ package algorithmsweek3.codingproblems;
 import java.util.Scanner;
 
 public class FractionalKnapsack {
-    private static double getOptimalValue(int capacity, int[] values, int[] weights) {
+
+    private static double getOptimalValue(int capacity, KnapsackItem[] kItems) {
         double value = 0;
         //write your code here
+        for (int i = 0; i < kItems.length; i++) {
+           System.out.println("Printing Item: " + i + " value: " + kItems[i].getItemValue());
+           value += kItems[i].getItemValue();
+        }
+        // sort the array by itemValue in descending order
+
+        System.out.println("Final added value: " + value + " the capacity: " + capacity);
 
         return value;
     }
@@ -14,13 +22,37 @@ public class FractionalKnapsack {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int capacity = scanner.nextInt();
-        scanner.close();
-        int[] values = new int[n];
-        int[] weights = new int[n];
+        KnapsackItem [] items = new KnapsackItem[n];
+
         for (int i = 0; i < n; i++) {
-            values[i] = scanner.nextInt();
-            weights[i] = scanner.nextInt();
+            // get value first and then weight for the item
+            items[i] = new KnapsackItem(scanner.nextInt(), scanner.nextInt());
         }
-        System.out.println(getOptimalValue(capacity, values, weights));
+        scanner.close();
+        System.out.println(getOptimalValue(capacity, items));
+    }
+
+    private static class KnapsackItem {
+        private double value;
+        private double weight;
+        private double itemValue;
+
+        public KnapsackItem(int value, int weight){
+            this.value = value;
+            this.weight = weight;
+            itemValue = getValue() / getWeight();
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        public double getWeight() {
+            return weight;
+        }
+
+        public double getItemValue() {
+            return itemValue;
+        }
     }
 }
